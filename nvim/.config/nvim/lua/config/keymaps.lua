35b95b76a6d4bd6a107065639a972827ec8555ab
MMMMMMNM <wwwwwwnm@outlook.com>
2025-02-22 19:10:19 +0800

    qwq

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -11,4 +11,4 @@ keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", opt)
 keymap.set("n", "<leader>sw", "<cmd>vsplit<cr>", opt)
 keymap.set("n", "<leader>ss", "<cmd>split<cr>", opt)
 
-keymap.set("n", "<leader>aa", "<cmd>ASToggle<cr>", opt)
+keymap.set("n", "<leader>am", "<cmd>ASToggle<cr>", opt)
--- nvim/.config/nvim/lua/plugins/snack.lua
+++ nvim/.config/nvim/lua/plugins/snack.lua
@@ -8,7 +8,195 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
-		dashboard = { enabled = true },
+		image = {
+			formats = {
+				"png",
+				"jpg",
+				"jpeg",
+				"gif",
+				"bmp",
+				"webp",
+				"tiff",
+				"heic",
+				"avif",
+				"mp4",
+				"mov",
+				"avi",
+				"mkv",
+				"webm",
+				"pdf",
+			},
+			force = false, -- try displaying the image, even if the terminal does not support it
+			doc = {
+				-- enable image viewer for documents
+				-- a treesitter parser must be available for the enabled languages.
+				enabled = true,
+				-- render the image inline in the buffer
+				-- if your env doesn't support unicode placeholders, this will be disabled
+				-- takes precedence over `opts.float` on supported terminals
+				inline = true,
+				-- render the image in a floating window
+				-- only used if `opts.inline` is disabled
+				float = true,
+				max_width = 80,
+				max_height = 40,
+				-- Set to `true`, to conceal the image text when rendering inline.
+				conceal = false, -- (experimental)
+			},
+			img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
+			-- window options applied to windows displaying image buffers
+			-- an image buffer is a buffer with `filetype=image`
+			wo = {
+				wrap = false,
+				number = false,
+				relativenumber = false,
+				cursorcolumn = false,
+				signcolumn = "no",
+				foldcolumn = "0",
+				list = false,
+				spell = false,
+				statuscolumn = "",
+			},
+			cache = vim.fn.stdpath("cache") .. "/snacks/image",
+			debug = {
+				request = false,
+				convert = false,
+				placement = false,
+			},
+			env = {},
+			---@class snacks.image.convert.Config
+			convert = {
+				notify = true, -- show a notification on error
+				---@type snacks.image.args
+				mermaid = function()
+					local theme = vim.o.background == "light" and "neutral" or "dark"
+					return { "-i", "{src}", "-o", "{file}", "-b", "transparent", "-t", theme, "-s", "{scale}" }
+				end,
+				---@type table<string,snacks.image.args>
+				magick = {
+					default = { "{src}[0]", "-scale", "1920x1080>" }, -- default for raster images
+					vector = { "-density", 192, "{src}[0]" }, -- used by vector images like svg
+					math = { "-density", 192, "{src}[0]", "-trim" },
+					pdf = { "-density", 192, "{src}[0]", "-background", "white", "-alpha", "remove", "-trim" },
+				},
+			},
+			math = {
+				enabled = true, -- enable math expression rendering
+				-- in the templates below, `${header}` comes from any section in your document,
+				-- between a start/end header comment. Comment syntax is language-specific.
+				-- * start comment: `// snacks: header start`
+				-- * end comment:   `// snacks: header end`
+				typst = {
+					tpl = [[
+        #set page(width: auto, height: auto, margin: (x: 2pt, y: 2pt))
+        #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
+        #set text(size: 12pt, fill: rgb("${color}"))
+        ${header}
+        ${content}]],
+				},
+				latex = {
+					font_size = "Large", -- see https://www.sascha-frank.com/latex-font-size.html
+					-- for latex documents, the doc packages are included automatically,
+					-- but you can add more packages here. Useful for markdown documents.
+					packages = { "amsmath", "amssymb", "amsfonts", "amscd", "mathtools" },
+					tpl = [[
+        \documentclass[preview,border=2pt,varwidth,12pt]{standalone}
+        \usepackage{${packages}}
+        \begin{document}
+        ${header}
+        { \${font_size} \selectfont
+          \color[HTML]{${color}}
+        ${content}}
+        \end{document}]],
+				},
+			},
+		},
+		dashboard = {
+			width = 60,
+			row = nil, -- dashboard position. nil for center
+			col = nil, -- dashboard position. nil for center
+			pane_gap = 4, -- empty columns between vertical panes
+			autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+			-- These settings are used by some built-in sections
+			preset = {
+				-- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+				---@type fun(cmd:string, opts:table)|nil
+				pick = nil,
+				-- Used by the `keys` section to show keymaps.
+				-- Set your custom keymaps here.
+				-- When using a function, the `items` argument are the default keymaps.
+				---@type snacks.dashboard.Item[]
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				-- Used by the `header` section
+				header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+			},
+			-- item field formatters
+			formats = {
+				icon = function(item)
+					if item.file and item.icon == "file" or item.icon == "directory" then
+						return M.icon(item.file, item.icon)
+					end
+					return { item.icon, width = 2, hl = "icon" }
+				end,
+				footer = { "%s", align = "center" },
+				header = { "%s", align = "center" },
+				file = function(item, ctx)
+					local fname = vim.fn.fnamemodify(item.file, ":~")
+					fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+					if #fname > ctx.width then
+						local dir = vim.fn.fnamemodify(fname, ":h")
+						local file = vim.fn.fnamemodify(fname, ":t")
+						if dir and file then
+							file = file:sub(-(ctx.width - #dir - 2))
+							fname = dir .. "/…" .. file
+						end
+					end
+					local dir, file = fname:match("^(.*)/(.+)$")
+					return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
+				end,
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
 		input = { enabled = true },
 		notifier = { enabled = true },
 	},
